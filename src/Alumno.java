0ce57283b1b124993a94ac84d671f63c48081db2
papulol <isidrolopezlinares@gmail.com>
2026-03-05 08:56:27 +0100

    Constructor

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,9 +1,17 @@
 public class Alumno {
-
-    public class Alumnos(){
+// Declarar atributos
+    public class Alumnos {
         private int id;
         private String nombre;
         private String tituloFP;
         private int edad;
 }
+// Metodo constructor
+    public Alumno(int id, String nombre, String tituloFP, int edad){
 
+        this.id = id;
+        this.nombre = nombre;
+        this.tituloFP = tituloFP;
+        this.edad = edad;
+    }
+}
