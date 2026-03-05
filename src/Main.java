69adc0944b8ac75491fbdf8b6bfdc587149e9b16
papulol <isidrolopezlinares@gmail.com>
2026-03-05 09:55:58 +0100

    Alumnos y resultado final Main

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,14 @@ public class Main {
 
         Alumno alumno1;
         Alumno alumno2;
+        Alumno alumno3;
+
+        PiniponLightYear = new CentroFP(20);
+
+        // Nuevos alumnos
+        alumno1 = new Alumno(1,"Mariete Chupete Jalao", "DAM", 1);
+        alumno2 = new Alumno(2,"Victorcin el bailarin", "ASIR", 19);
+        alumno3 = new Alumno(3, "Luca Chiclauri", "DAW", 20)
+
+        System.out.println("El número total de alumnos es: " + );
         {
