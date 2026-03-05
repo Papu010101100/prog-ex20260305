c9ef0f2e27c1d298b29d5f5d32e70d220acb0336
papulol <isidrolopezlinares@gmail.com>
2026-03-05 09:39:00 +0100

    Metodo buscar Alumno CentroFp

--- src/CentroFP.java
+++ src/CentroFP.java
@@ -9,4 +9,17 @@ public CentroFP (int maximo_alumnos){
     MAX_ALUMNOS = maximo_alumnos ;
     alumnos = new Alumno[MAX_ALUMNOS];
 }
+// Metodo buscar alumno
+public Alumno buscarAlumno (int idAlumno){
+    boolean seguirBuscando = true;
+    Alumno AlumnoEncontrado = null;
+// Bucle buscar alumno
+    for (int i = 0; i < MAX_ALUMNOS && seguirBuscando; i++) {
+        if( alumnos[i] != null && alumnos[i].getId() == idAlumno ){
+            seguirBuscando = false;
+            alumnoEncontrado = alumnos[i];
+        }
+    }
 
+    return alumnoEncontrado;
+}
